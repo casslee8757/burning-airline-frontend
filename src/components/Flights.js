4edf74e6bb3dd49834983285436ce6dcc2b6f404
“Cassie <cassielee8757@gmail.com>
2022-02-16 23:26:04 +1100

    git pull request

--- src/components/Flights.js
+++ src/components/Flights.js
@@ -3,6 +3,11 @@ import axios from "axios";
 
 class Flights extends React.Component{
 
+  state = {
+   flights: [],
+   error: null
+  }
+
   
   componentDidMount(){
     const params = this.props.match.params
@@ -13,17 +18,34 @@ class Flights extends React.Component{
     try {
       console.log(origin, destination);
       const res = await axios.get('http://localhost:3000/flights/search', {params: {origin: origin, destination: destination}});
-      console.log('response', res);
+      console.log('response', res); 
+      this.setState({ flights: res.data });
       
     } catch( err ){
       console.log('Error in search AJAX: ', err);
+      this.setState({ error: err });
+
     }
   }
 
   render(){
+
+    const flights = this.state;
+
+
     return(
       <div>
         <p>hello</p>
+
+        
+        <p>Destination</p>
+          
+
+        
+
+
+
+
       </div>
     );
   } // render()
