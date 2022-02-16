a72d2dbb82f35d940d39e79857839e80f2614929
Steven Furnari <s.furnari@hotmail.com>
2022-02-16 23:22:58 +1100

    working on flights table

--- src/components/Flights.js
+++ src/components/Flights.js
@@ -3,6 +3,11 @@ import axios from "axios";
 
 class Flights extends React.Component{
 
+  state = {
+    plane: [],
+    flight: [],
+  }
+
   
   componentDidMount(){
     const params = this.props.match.params
@@ -12,8 +17,11 @@ class Flights extends React.Component{
   performSearch = async (origin, destination) => {
     try {
       console.log(origin, destination);
-      const res = await axios.get('http://localhost:3000/flights/search', {origin: origin, destination: destination});
-      console.log('response', res);
+      const res = await axios.get('http://localhost:3000/flights/search', {params: {origin: origin, destination: destination}});
+      console.log(res.data);
+      console.log(res.data.plane[0].name);
+
+      this.setState({flight: res.data.flight, plane: res.data.plane})
       
     } catch( err ){
       console.log('Error in search AJAX: ', err);
@@ -23,7 +31,26 @@ class Flights extends React.Component{
   render(){
     return(
       <div>
-        <p>hello</p>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Flight</th>
+              <th>From {">"} To</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.flight.map(f => (
+                <tr>
+                  <td>{f.date_flight}</td>
+                  <td>BA-0{f.id}</td>
+                  <td>{f.origin} {">"} {f.destination}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
       </div>
     );
   } // render()
